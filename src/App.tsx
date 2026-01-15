28ca18b558e25175bd6bc2f20fcff72b0a42695f
Patrick Oser <patrick.oser148@gmail.com>
2026-01-15 17:36:45 -0500

    Did it.

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,25 @@ import { useEffect, useState } from 'react'
 
 function App() {
   const [imgURL, setImgURL] = useState()
+  const [userQ, setUserQ] = useState()
 
   const handleRandomBtn = async () => {
     try {
       const response = await fetch('https://api.giphy.com/v1/gifs/random?api_key=ADJhmHFAajZpCZaIVh5myHPaj6LMAdkJ&tag=dogs&rating=g')
       const data = await response.json()
-      console.log(data)
       setImgURL(data.data.images.original.url)
     } catch(err) {
+      console.error(err)
+    }
+  }
+
+  const handleSearchBtn = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=ADJhmHFAajZpCZaIVh5myHPaj6LMAdkJ&q=${userQ}&limit=5&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
+      const data = await response.json()
+      console.log(data)
+    } catch (err) {
       console.error(err)
     }
   }
@@ -21,7 +32,6 @@ function App() {
         return response.json()
       })
       .then(function(response) {
-        console.log(response.data.images.original.url)
         setImgURL(response.data.images.original.url)
       })
   }, [])
@@ -31,6 +41,11 @@ function App() {
       <h1>
         Weather App
       </h1>
+      <form onSubmit={(e) => handleSearchBtn(e)} action="">
+        <input value={userQ} onChange={(e) => setUserQ(e.target.value)} type="search" name="search" id="search" />
+        <button type='submit'>Search</button>
+      </form>
+
       <img src={imgURL} alt="" />
       <button onClick={handleRandomBtn}>Random</button>
     </>
