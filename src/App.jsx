861b2a06e9014f531182ef2f8841cf274f9af0a9
Patrick Oser <patrick.oser148@gmail.com>
2026-01-20 12:33:37 -0500

    Remove ts.

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,30 @@ import './App.css'
 import { useEffect, useState } from 'react'
 
 function App() {
-  const [imgURL, setImgURL] = useState()
-  const [userQ, setUserQ] = useState()
+  const [imgURL, setImgURL] = useState('')
+  const [userQ, setUserQ] = useState('')
+  const [searchResults, setSearchResults] = useState([])
 
   const handleRandomBtn = async () => {
     try {
       const response = await fetch('https://api.giphy.com/v1/gifs/random?api_key=ADJhmHFAajZpCZaIVh5myHPaj6LMAdkJ&tag=dogs&rating=g')
       const data = await response.json()
-      setImgURL(data.data.images.original.url)
+      setImgURL(data.data.map((img) => images.original.url))
     } catch(err) {
       console.error(err)
     }
   }
 
-  const handleSearchBtn = async (e) => {
+  const handleSearchChange = (e) => {
+    setUserQ(e.target.value)
+  }
+  const handleSearchBtnQ = async (e) => {
     e.preventDefault()
     try {
       const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=ADJhmHFAajZpCZaIVh5myHPaj6LMAdkJ&q=${userQ}&limit=5&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
       const data = await response.json()
       console.log(data)
+      setSearchResults(data.data)
     } catch (err) {
       console.error(err)
     }
@@ -41,12 +46,21 @@ function App() {
       <h1>
         Weather App
       </h1>
-      <form onSubmit={(e) => handleSearchBtn(e)} action="">
-        <input value={userQ} onChange={(e) => setUserQ(e.target.value)} type="search" name="search" id="search" />
+      <form onSubmit={(e) => handleSearchBtnQ(e)} action="">
+        <input value={userQ} onChange={handleSearchChange} type="search" name="search" id="search" />
         <button type='submit'>Search</button>
       </form>
+      {!searchResults ?
+        <div>
+          No search results
+        </div>
+      : 
+        <div>
+          {searchResults.map(img => img.images.original.url)}
+        </div>
+      }
 
-      <img src={imgURL} alt="" />
+      {imgURL && <img src={imgURL} alt="" />}
       <button onClick={handleRandomBtn}>Random</button>
     </>
   )
