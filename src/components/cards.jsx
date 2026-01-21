d40578d21a61a0fe88fbbed61b937d0277409d5e
Patrick Oser <patrick.oser148@gmail.com>
2026-01-21 13:37:40 -0500

    Style.

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -3,20 +3,20 @@ import React from 'react';
 const Cards = () => {
 
     return (
-        <div>
-            <article>
+        <div className='cards-container'>
+            <article className='cards'>
                 <h2>Today</h2>
                 <p className="temp"></p>
                 <p className="precip"></p>
                 <p className="wind"></p>
             </article>
-            <article>
+            <article className='cards'>
                 <h2>Tommorrow</h2>
                 <p className="temp"></p>
                 <p className="precip"></p>
                 <p className="wind"></p>
             </article>
-            <article>
+            <article className='cards'>
                 <h2>The Day After Tomorrow</h2>
                 <p className="temp"></p>
                 <p className="precip"></p>
