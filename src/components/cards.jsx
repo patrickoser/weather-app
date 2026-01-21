c512283ad908f16a021eaac9f59fbeecce935c44
Patrick Oser <patrick.oser148@gmail.com>
2026-01-21 11:36:25 -0500

    Add elements to Cards.

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -4,7 +4,24 @@ const Cards = () => {
 
     return(
         <div>
-
+            <article>
+                <h2>Today</h2>
+                <p classname="temp"></p>
+                <p classname="precip"></p>
+                <p classname="wind"></p>
+            </article>
+            <article>
+                <h2>Tommorrow</h2>
+                <p classname="temp"></p>
+                <p classname="precip"></p>
+                <p classname="wind"></p>
+            </article>
+            <article>
+                <h2>The Day After Tomorrow</h2>
+                <p classname="temp"></p>
+                <p classname="precip"></p>
+                <p classname="wind"></p>
+            </article>
         </div>
     )
 }
