a964f6e755b01288a36378a9f1fd2f0ada7011cb
Patrick Oser <patrick.oser148@gmail.com>
2026-01-21 14:17:19 -0500

    Add better element structure.

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -6,21 +6,27 @@ const Cards = () => {
         <div className='cards-container'>
             <article className='cards'>
                 <h2>Today</h2>
-                <p className="temp"></p>
-                <p className="precip"></p>
-                <p className="wind"></p>
+                <div className="weather-details">
+                    <p className="temp"></p>
+                    <p className="precip"></p>
+                    <p className="wind"></p>
+                </div>
             </article>
             <article className='cards'>
                 <h2>Tommorrow</h2>
-                <p className="temp"></p>
-                <p className="precip"></p>
-                <p className="wind"></p>
+                <div className="weather-details">
+                    <p className="temp"></p>
+                    <p className="precip"></p>
+                    <p className="wind"></p>
+                </div>
             </article>
             <article className='cards'>
                 <h2>The Day After Tomorrow</h2>
-                <p className="temp"></p>
-                <p className="precip"></p>
-                <p className="wind"></p>
+                <div className="weather-details">
+                    <p className="temp"></p>
+                    <p className="precip"></p>
+                    <p className="wind"></p>
+                </div>
             </article>
         </div>
     )
