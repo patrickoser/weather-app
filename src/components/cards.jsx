9195755f1c36a8c31879f501252e45c0b3834e67
Patrick Oser <patrick.oser148@gmail.com>
2026-01-21 16:07:06 -0500

    Add state variables and conditional checks to Cards.

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,34 +1,56 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Cards = () => {
+    const [searchLocation, setSearchLocation] = useState('')
+    const [weatherData, setWeatherData] = useState({
+        currentTemp: null,
+        maxTemp: null,
+        minTemp: null,
+        precipitation: null,
+        windSpeed: null
+    })
+
+    const getData = async () => {
+        const res = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/')
+    }
 
     return (
-        <div className='cards-container'>
-            <article className='cards'>
-                <h2>Today</h2>
-                <div className="weather-details">
-                    <p className="temp">Temp</p>
-                    <p className="precip">Precip</p>
-                    <p className="wind">Wind</p>
-                </div>
-            </article>
-            <article className='cards'>
-                <h2>Tommorrow</h2>
-                <div className="weather-details">
-                    <p className="temp">Temp</p>
-                    <p className="precip">Precip</p>
-                    <p className="wind">Wind</p>
-                </div>
-            </article>
-            <article className='cards'>
-                <h2>The Day After Tomorrow</h2>
-                <div className="weather-details">
-                    <p className="temp">Temp</p>
-                    <p className="precip">Precip</p>
-                    <p className="wind">Wind</p>
-                </div>
-            </article>
-        </div>
+        weatherData.currentTemp ? (
+            <div className='cards-container'>
+                <article className='cards'>
+                    <h2>Today</h2>
+                    <div className="weather-details">
+                        <p className="current-temp">{weatherData.currentTeamp}</p>
+                        <p className="max-temp">{weatherData.maxTemp}</p>
+                        <p className="min-temp">{weatherData.minTemp}</p>
+                        <p className="precip">{weatherData.precipitation}</p>
+                        <p className="wind">{weatherData.windSpeed}</p>
+                    </div>
+                </article>
+                <article className='cards'>
+                    <h2>Tommorrow</h2>
+                    <div className="weather-details">
+                        <p className="current-temp">{weatherData.currentTeamp}</p>
+                        <p className="max-temp">{weatherData.maxTemp}</p>
+                        <p className="min-temp">{weatherData.minTemp}</p>
+                        <p className="precip">{weatherData.precipitation}</p>
+                        <p className="wind">{weatherData.windSpeedWind}</p>
+                    </div>
+                </article>
+                <article className='cards'>
+                    <h2>The Day After Tomorrow</h2>
+                    <div className="weather-details">
+                        <p className="current-temp">{weatherData.currentTeamp}</p>
+                        <p className="max-temp">{weatherData.maxTemp}</p>
+                        <p className="min-temp">{weatherData.minTemp}</p>
+                        <p className="precip">{weatherData.precipitation}</p>
+                        <p className="wind">{weatherData.windSpeedWind}</p>
+                    </div>
+                </article>
+            </div>
+        ) : 
+            <h2>Input a Location.</h2>
     )
 }
 
