778f5886c3ed65f11b1185d832db78012bd0e88a
Patrick Oser <patrick.oser148@gmail.com>
2026-01-23 18:56:42 -0500

    Update Cards to reflect data and Readme.

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Context:
   - Context is not entirely necessary but I want to use it for practice/refresher, same goes for some light prop drilling. Could just grab location/searchData in App and prop drill the values to Form/Cards. Use the COntext file for all useEffects and other state that needs to be used in either component.
 
 Currently:
-  - searchData is generated appropriately and data is fetched I just have to access it now, currently not showing up as a workable object.
+  - Weather data is passed to the cards but it is all hard coded and should be generated dynamically depending on the length of the days array in weatherData.
+  - Having a current temp for all days doesnt make sense since there is not current temp for tomorrow and the next day. I could change which data point gets pulled to the average for the day or do a check so that those days only show the average instead of the currenttemp. Though for those future days it might default to the data point being the average, or maybe its the projected tempature at this time 24/48 hours from now, because otherwise it wouldnt have anything to give me to begin with.
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -7,39 +7,39 @@ const Cards = () => {
     const { searchData, setSearchData, weatherData } = useContext(DataContext)
 
     return (
-        weatherData?.currentTemp ? (
+        weatherData?.days[0].currentTemp ? (
             <div className='cards-container'>
                 <article className='cards'>
                     <h2>Today</h2>
                     <div className="weather-details">
-                        <p className="current-temp">{weatherData.currentTeamp}</p>
-                        <p className="max-temp">{weatherData.maxTemp}</p>
-                        <p className="min-temp">{weatherData.minTemp}</p>
-                        <p className="humidity">{weatherData.humidity}</p>
-                        <p className="wind">{weatherData.windSpeed}</p>
-                        <p className='description'>{weatherData.description}</p>
+                        <p className="current-temp">{weatherData.days[0].currentTemp}</p>
+                        <p className="max-temp">{weatherData.days[0].maxTemp}</p>
+                        <p className="min-temp">{weatherData.days[0].minTemp}</p>
+                        <p className="humidity">{weatherData.days[0].humidity}</p>
+                        <p className="wind">{weatherData.days[0].windSpeed}</p>
+                        <p className='description'>{weatherData.days[0].description}</p>
                     </div>
                 </article>
                 <article className='cards'>
                     <h2>Tommorrow</h2>
                     <div className="weather-details">
-                        <p className="current-temp">{weatherData.currentTeamp}</p>
-                        <p className="max-temp">{weatherData.maxTemp}</p>
-                        <p className="min-temp">{weatherData.minTemp}</p>
-                        <p className="humidity">{weatherData.humidity}</p>
-                        <p className="wind">{weatherData.windSpeed}</p>
-                        <p className='description'>{weatherData.description}</p>
+                        <p className="current-temp">{weatherData.days[1].currentTemp}</p>
+                        <p className="max-temp">{weatherData.days[1].maxTemp}</p>
+                        <p className="min-temp">{weatherData.days[1].minTemp}</p>
+                        <p className="humidity">{weatherData.days[1].humidity}</p>
+                        <p className="wind">{weatherData.days[1].windSpeed}</p>
+                        <p className='description'>{weatherData.days[1].description}</p>
                     </div>
                 </article>
                 <article className='cards'>
                     <h2>The Day After Tomorrow</h2>
                     <div className="weather-details">
-                        <p className="current-temp">{weatherData.currentTeamp}</p>
-                        <p className="max-temp">{weatherData.maxTemp}</p>
-                        <p className="min-temp">{weatherData.minTemp}</p>
-                        <p className="humidity">{weatherData.humidity}</p>
-                        <p className="wind">{weatherData.windSpeed}</p>
-                        <p className='description'>{weatherData.description}</p>
+                        <p className="current-temp">{weatherData.days[2].currentTemp}</p>
+                        <p className="max-temp">{weatherData.days[2].maxTemp}</p>
+                        <p className="min-temp">{weatherData.days[2].minTemp}</p>
+                        <p className="humidity">{weatherData.days[2].humidity}</p>
+                        <p className="wind">{weatherData.days[2].windSpeed}</p>
+                        <p className='description'>{weatherData.days[2].description}</p>
                     </div>
                 </article>
             </div>
