d041d5c71018e41585e757ac74f7c8b6a234f5cf
Patrick Oser <patrick.oser148@gmail.com>
2026-01-23 18:45:33 -0500

    Fix state update issue.

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -8,37 +8,26 @@ export const DataProvider = ({ children }) => {
         startDate: null,
         endDate: null
     })
-    const [weatherData, setWeatherData] = useState({
-        currentTemp: null,
-        maxTemp: null,
-        minTemp: null,
-        humidity: null,
-        windSpeed: null,
-        description: null
-    })
+    const [weatherData, setWeatherData] = useState({ days: [] })
 
     const getData = async () => { 
         
         try {
             const res = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchData.location}/${searchData.startDate}/${searchData.endDate}?key=92TP9PDYW45DZ53CUVY4DBWDC`)
             const data = await res.json()
-            console.log('Data fetched: ', data.days)
-            /* setWeatherData({
-                currentTemp: data.days[0].temp,
-                maxTemp: data.days[0].tempmax,
-                minTemp: data.days[0].tempmin,
-                humidity: data.days[0].humidity,
-                windSpeed: data.days[0].windspeed,
-                description: data.days[0].description
-            })*/
-           setWeatherData(data.days?.map((day) => ({
-            currentTemp: day.temp,
-            maxTemp: day.tempmax,
-            minTemp: day.tempmin,
-            humidity: day.humidity,
-            windSpeed: day.windspeed,
-            description: day.description
-            })))
+            console.log('Data fetched: ', data)
+
+            const mappedData = data.days?.map((day) => ({
+                currentTemp: day.temp,
+                maxTemp: day.tempmax,
+                minTemp: day.tempmin,
+                humidity: day.humidity,
+                windSpeed: day.windspeed,
+                description: day.description
+            }))
+            console.log(mappedData)
+
+            setWeatherData({ days: mappedData })
         } catch (err) {
             console.error(err.message)
         }
@@ -48,8 +37,12 @@ export const DataProvider = ({ children }) => {
         if (searchData.location) {
             getData()
         }
+
     }, [searchData.location])
 
+    useEffect(() => {
+        console.log(weatherData)
+    }, [weatherData.days[0, 1, 2]])
     return (
         <DataContext.Provider value={{ searchData, setSearchData, getData, weatherData }}>
             {children}
