490df3f5e77011dea273a39609901d2d2fb9e60a
Patrick Oser <patrick.oser148@gmail.com>
2026-01-24 16:54:37 -0500

    Cards generate dynamically.

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -8,53 +8,26 @@ const Cards = () => {
 
     return (
         weatherData?.days[0]?.currentTemp ? (
-            <div>
+            <div className="cards-container">
                 {weatherData.days.map((day, index) => (
-                    <article className="weather-details" key={index}>
-                        (weatherData.days[day] === 0 ? <p className="current-temp">Current Temp: {weatherData.days[day].currentTemp}</p> : <p className="average-temp">Avg Temp: {weatherData.days[day].currentTemp}</p>)
-                        <p className="max-temp">Max Temp: {weatherData.days[day].maxTemp}</p>
-                        <p className="min-temp">Min Temp: {weatherData.days[day].minTemp}</p>
-                        <p className="humidity">Humidity: {weatherData.days[day].humidity}</p>
-                        <p className="wind">Wind Speed: {weatherData.days[day].windSpeed}</p>
-                        <p className='description'>Decription: {weatherData.days[day].description}</p>
+                    <article className="cards" key={index}>
+                        {index === 0 ?
+                            (<h2>Today</h2>) :
+                        index === 1 ? 
+                            (<h2>Tomrrow</h2>) :
+                            (<h2>Day After Tomrrow</h2>)}
+                        {index === 0 ? 
+                            (<p className="current-temp">Current Temp: {day.currentTemp}</p>) : 
+                            (<p className="average-temp">Avg Temp: {day.currentTemp}</p>)
+                        }
+                        <p className="max-temp">Max Temp: {day.maxTemp}</p>
+                        <p className="min-temp">Min Temp: {day.minTemp}</p>
+                        <p className="humidity">Humidity: {day.humidity}</p>
+                        <p className="wind">Wind Speed: {day.windSpeed}</p>
+                        <p className='description'>Decription: {day.description}</p>
                     </article>
                 ))}
             </div>
-            /*<div className='cards-container'>
-                <article className='cards'>
-                    <h2>Today</h2>
-                    <div className="weather-details">
-                        <p className="current-temp">{weatherData.days[0].currentTemp}</p>
-                        <p className="max-temp">{weatherData.days[0].maxTemp}</p>
-                        <p className="min-temp">{weatherData.days[0].minTemp}</p>
-                        <p className="humidity">{weatherData.days[0].humidity}</p>
-                        <p className="wind">{weatherData.days[0].windSpeed}</p>
-                        <p className='description'>{weatherData.days[0].description}</p>
-                    </div>
-                </article>
-                <article className='cards'>
-                    <h2>Tommorrow</h2>
-                    <div className="weather-details">
-                        <p className="current-temp">{weatherData.days[1].currentTemp}</p>
-                        <p className="max-temp">{weatherData.days[1].maxTemp}</p>
-                        <p className="min-temp">{weatherData.days[1].minTemp}</p>
-                        <p className="humidity">{weatherData.days[1].humidity}</p>
-                        <p className="wind">{weatherData.days[1].windSpeed}</p>
-                        <p className='description'>{weatherData.days[1].description}</p>
-                    </div>
-                </article>
-                <article className='cards'>
-                    <h2>The Day After Tomorrow</h2>
-                    <div className="weather-details">
-                        <p className="current-temp">{weatherData.days[2].currentTemp}</p>
-                        <p className="max-temp">{weatherData.days[2].maxTemp}</p>
-                        <p className="min-temp">{weatherData.days[2].minTemp}</p>
-                        <p className="humidity">{weatherData.days[2].humidity}</p>
-                        <p className="wind">{weatherData.days[2].windSpeed}</p>
-                        <p className='description'>{weatherData.days[2].description}</p>
-                    </div>
-                </article>
-            </div>*/
         ) : <h2>Input a Location.</h2>
     )
 }
