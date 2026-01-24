5b9561c13e0dc9a4283e760b9b91b2356f54a373
Patrick Oser <patrick.oser148@gmail.com>
2026-01-24 16:55:00 -0500

    Delete unused imports.

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import { DataContext } from '../context/DataContext';
 
 const Cards = () => {
