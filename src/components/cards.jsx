1f722314957ea416a21929074544aa02ab279624
Patrick Oser <patrick.oser148@gmail.com>
2026-01-25 11:27:57 -0500

    Comments.

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -9,13 +9,17 @@ const Cards = () => {
     return (
         weatherData?.days[0]?.currentTemp ? (
             <div className="cards-container">
+                {/* Call map on the days array in weatherData, day parameter for each day object
+                and index for react to track with key attribute. */}
                 {weatherData.days.map((day, index) => (
                     <article className="cards" key={index}>
+                        {/* Chained ternary to decide which title to display. */}
                         {index === 0 ?
                             (<h2>Today</h2>) :
                         index === 1 ? 
                             (<h2>Tomrrow</h2>) :
                             (<h2>Day After Tomrrow</h2>)}
+                        {/* Ternary to decide which text to display. */}
                         {index === 0 ? 
                             (<p className="current-temp">Current Temp: {day.currentTemp}</p>) : 
                             (<p className="average-temp">Avg Temp: {day.currentTemp}</p>)
